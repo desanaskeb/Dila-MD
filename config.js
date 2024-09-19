0fc87cd021d388d78bbc0ef33634f69434a7e4d6
desanaskeb <des.anas.keb@gmail.com>
2024-09-20 00:58:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "IClEVTxa#rXEKWLfEh7yX2kFTdJj47LaI6vj5bkFoOd4R6v0jsB8",
-MONGODB: process.env.MONGODB || "",
+MONGODB: process.env.MONGODB || "mongodb+srv://anasmodz:733670872@cluster0.2k7nnnm.mongodb.net/?retryWrites=true&w=majority",
 };
